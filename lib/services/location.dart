993b294486ccd67100281e8b5cca9fe8d0c7cc2b
MateusAkiraMuranaka <akira.kazoku@gmail.com>
2023-09-28 21:36:54 -0300

    Commit para Salvar Estado Atual

---
+++ lib/services/location.dart
@@ -0,0 +1,14 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double? latitude;
+  double? longitude;
+
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+    );
+    latitude = position.latitude;
+    longitude = position.longitude;
+  }
+}
